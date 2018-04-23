c2c5f93170c13ae5c46a10de2c3fa5d65106e568
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Models/MyBinder.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports DevExpress.Web.Mvc
+Imports System.ComponentModel
+Imports System.Web.Mvc
+
+Public Class MyEditorsBinder
+	Inherits DevExpressEditorsBinder
+	Protected Overrides Function GetPropertyValue(ByVal controllerContext As ControllerContext, ByVal bindingContext As ModelBindingContext, ByVal propertyDescriptor As PropertyDescriptor, ByVal propertyBinder As IModelBinder) As Object
+		If propertyDescriptor.PropertyType Is GetType(CommonMeals) Then
+			Dim tc As TypeConverter = propertyDescriptor.Converter
+			Return tc.ConvertFrom(EditorExtension.GetValue(Of String)(propertyDescriptor.Name))
+		End If
+
+		Return MyBase.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder)
+	End Function
+End Class
+
---
+++ VB/Models/MyModel.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.ComponentModel
+
+Public Enum CommonMeals
+	Breakfast
+	Lunch
+	Dinner
+	Supper
+End Enum
+
+Public Class MyModel
+	Private privateName As String
+	Public Property Name() As String
+		Get
+			Return privateName
+		End Get
+		Set(ByVal value As String)
+			privateName = value
+		End Set
+	End Property
+	Private privateMeal As CommonMeals
+	Public Property Meal() As CommonMeals
+		Get
+			Return privateMeal
+		End Get
+		Set(ByVal value As CommonMeals)
+			privateMeal = value
+		End Set
+	End Property
+End Class
